ae8999199bb4d6e547a24ef2dff9498b3d72fbec
chenyaoyang2019 <879091922@qq.com>
2022-08-08 11:47:18 +0800

    mall-tiny-02

---
+++ mall-tiny-02/src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
@@ -0,0 +1,112 @@
+package com.macro.mall.tiny.controller;
+
+import com.macro.mall.tiny.common.api.CommonPage;
+import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.mbg.model.PmsBrand;
+import com.macro.mall.tiny.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author: chenyaoyang
+ * @date: 2022/8/6 17:12
+ * @description: 品牌管理
+ **/
+@Controller
+@RequestMapping("/brand")
+//@Api(tags = "PmsBrandController", description = "商品品牌管理")
+public class PmsBrandController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
+
+    @Autowired
+    private PmsBrandService brandService;
+
+    @ApiOperation("获取所有品牌列表")
+    @RequestMapping(value = "/listAll", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<PmsBrand>> getBrandList() {
+        return CommonResult.success(brandService.listAllBrand());
+    }
+
+    @ApiOperation("添加品牌")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult createBrand(@RequestBody PmsBrand brand) {
+        CommonResult commonResult;
+        int count = brandService.createBrand(brand);
+        if (count == 1) {
+            commonResult = CommonResult.success(brand);
+            LOGGER.debug("createBrand success:{}", brand);
+        } else {
+            commonResult = CommonResult.failed("操作失败");
+            LOGGER.debug("createBrand failed:{}", brand);
+        }
+
+        return commonResult;
+    }
+
+    @ApiOperation("更新指定id品牌信息")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updateBrand(@PathVariable("id") Long id, @RequestBody PmsBrand brand, BindingResult result) {
+        CommonResult commonResult;
+        int count = brandService.updateBrand(id, brand);
+        if (count == 1) {
+            commonResult = CommonResult.success(brand);
+            LOGGER.debug("updateBrand success:{}", brand);
+        } else {
+            commonResult = CommonResult.failed("操作失败");
+            LOGGER.debug("updateBrand failed:{}", brand);
+        }
+
+        return commonResult;
+    }
+
+    @ApiOperation("删除指定id的品牌")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult deleteBrand(@PathVariable("id") Long id) {
+        CommonResult commonResult;
+        int count = brandService.deleteBrand(id);
+        if (count == 1) {
+            commonResult = CommonResult.success(null);
+            LOGGER.debug("createBrand success: id={}", id);
+        } else {
+            commonResult = CommonResult.failed("操作失败");
+            LOGGER.debug("createBrand failed:id={}", id);
+        }
+
+        return commonResult;
+    }
+
+    @ApiOperation("分页查询品牌列表")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<CommonPage<PmsBrand>> listBrand(@RequestParam(value = "pageNum", defaultValue = "1")
+                                                        @ApiParam("页码") Integer pageNum,
+                                                        @RequestParam(value = "pageSize", defaultValue = "3")
+                                                        @ApiParam("每页数量") Integer pageSize) {
+        List<PmsBrand> brandList = brandService.listBrand(pageNum, pageSize);
+        return CommonResult.success(CommonPage.restPage(brandList));
+    }
+
+    @ApiOperation("获取指定id的品牌详情")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult brand(@PathVariable("id") Long id) {
+
+        return CommonResult.success(brandService.getBrand(id));
+    }
+
+
+}
